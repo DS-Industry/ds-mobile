bc0a64f4a7319c612f1533889e129ab2d5547738
adamsber34 <evg.shoming@gmail.com>
2022-11-07 03:27:20 +0300

    Added helmet

--- ds-mobile-auth/src/main.ts
+++ ds-mobile-auth/src/main.ts
@@ -6,6 +6,7 @@ import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 import * as compression from 'compression';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -13,20 +14,21 @@ async function bootstrap() {
   app.useGlobalFilters(
     new AllExceptionFilter(app.get(WINSTON_MODULE_NEST_PROVIDER)),
   );
-  app.setGlobalPrefix('/api/v1');
+  app.setGlobalPrefix('auth/api/v1');
   app.use(compression());
+  app.use(helmet());
   app.enable('trust proxy');
 
   //Swagger set up
   const options = new DocumentBuilder()
     .setTitle(`${process.env.APP_NAME}`)
-    .setBasePath('/api/v1/')
+    .setBasePath('auth/api/v1/')
     .setDescription(`${process.env.APP_NAME} api`)
     .setVersion('1.0.0')
     .build();
 
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api/v1/docs', app, document);
+  SwaggerModule.setup('auth/api/v1/docs', app, document);
   await app.listen(process.env.APP_PORT || 3000);
 }
 bootstrap();
--- ds-mobile-core/src/main.ts
+++ ds-mobile-core/src/main.ts
@@ -5,6 +5,7 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import * as compression from 'compression';
 import { AllExceptionFilter } from './common/filters/all-exception.filter';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
   );
   app.setGlobalPrefix('core/api/v1');
   app.use(compression());
+  app.use(helmet());
   app.enable('trust proxy');
 
   await app.listen(process.env.APP_PORT || 5000);
