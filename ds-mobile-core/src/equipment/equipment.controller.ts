8def33f4397838ed657e56717a5731c4ea4d1a8d
adamsber34 <evg.shoming@gmail.com>
2022-11-16 11:26:40 +0300

    added auth for equipment module

--- ds-mobile-core/src/equipment/equipment.controller.ts
+++ ds-mobile-core/src/equipment/equipment.controller.ts
@@ -3,18 +3,30 @@ import { EquipmentService } from './equipment.service';
 import { StartEquipmentRequest } from './dto/req/start-equipment-request.dto';
 import { EquipmentExceptionFilter } from '../common/filters/equipment-exception.filter';
 import { SkipThrottle } from '@nestjs/throttler';
+import { RequestHeader } from '../common/decorators/request-header.decorator';
+import { CardHeader } from '../card/dto/req/card-header.dto';
+import { AuthService } from '../auth/auth.service';
 
 @Controller('external')
 export class EquipmentController {
-  constructor(private readonly equipmentService: EquipmentService) {}
+  constructor(
+    private readonly equipmentService: EquipmentService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('/start/:id')
   @SkipThrottle(true)
   @UseFilters(new EquipmentExceptionFilter())
   public async startEquipment(
+    @RequestHeader(CardHeader) headers: any,
     @Body() startEquipmentRequest: StartEquipmentRequest,
     @Param('id') id: string,
   ) {
+    const { accessToken } = headers;
+    await this.authService.verifyAccessToken(
+      startEquipmentRequest.GVLCardNum,
+      accessToken,
+    );
     return this.equipmentService.start(startEquipmentRequest, id);
   }
 }
--- ds-mobile-core/src/equipment/equipment.module.ts
+++ ds-mobile-core/src/equipment/equipment.module.ts
@@ -3,9 +3,10 @@ import { EquipmentService } from './equipment.service';
 import { EquipmentController } from './equipment.controller';
 import { HttpModule } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [HttpModule, ConfigModule],
+  imports: [AuthModule, HttpModule, ConfigModule],
   controllers: [EquipmentController],
   providers: [EquipmentService],
 })
