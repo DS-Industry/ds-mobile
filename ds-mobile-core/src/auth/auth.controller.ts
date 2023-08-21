c6d6b5b85c65f390ba15348b7b32a9fc32c7897e
adamsbear34 <es1230200306@gmail.com>
2023-08-21 13:11:42 +0300

    increased throttler for acces token route

--- ds-mobile-core/src/auth/auth.controller.ts
+++ ds-mobile-core/src/auth/auth.controller.ts
@@ -3,11 +3,13 @@ import { AuthService } from './auth.service';
 import { GetAccessTokenRequest } from './dto/req/get-access-token-request.dto';
 import { IPayloadJwt } from './interface/payload.interface';
 import { ApiKeyGuard } from './guard/api-key.guard';
+import { Throttle } from '@nestjs/throttler';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Throttle(20, 60)
   @Post('/access-token')
   @UseGuards(ApiKeyGuard)
   public async getAccessToken(
